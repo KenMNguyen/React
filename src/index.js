7019832b5fc43fa716843f6a4597a016aa264082
ken <ken@MK.local>
2020-10-22 01:57:59 -0700

    Header and Footer

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
 
 ReactDOM.render(
