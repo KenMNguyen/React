fa8d55ae3c9c7156124a908c183707fb8e957f5b
ken <ken@MK.local>
2020-11-06 23:44:12 -0800

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -27,7 +27,7 @@ function RenderCampsite({campsite}) {
 }
 
 // renderComments
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if(comments) {
         return(
             <div className='col-md-5 m-1'>
@@ -43,7 +43,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         </div>
                     );
                 })}
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );
     }
@@ -91,7 +91,7 @@ function CampsiteInfo(props) {
                 <RenderCampsite campsite={props.campsite} />
                 <RenderComments 
                     comments={props.comments}
-                    addComment={props.addComment}
+                    postComment={props.postComment}
                     campsiteId={props.campsite.id}
                 />
             </div>
@@ -120,7 +120,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
         // console.log('Current State is: ' + JSON.stringify(values));
         // alert('Current State is: ' + JSON.stringify(values));
     }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,33 +9,28 @@ import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { actions } from 'react-redux-form';
-import { addComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
+import { postComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
 
 
 
-// const mapStateToProps = state => {
-//     return {
-//         campsites: state.campsites,
-//         comments: state.comments,
-//         partners: state.partners,
-//         promotions: state.promotions
-//     };
-// };
+const mapStateToProps = state => {
+    return {
+        campsites: state.campsites,
+        comments: state.comments,
+        partners: state.partners,
+        promotions: state.promotions
+    };
+};
 
 //fetch
 const mapDispatchToProps = {
-    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
+    postComment: (campsiteId, rating, author, text) => (postComment(campsiteId, rating, author, text)),
     fetchCampsites: () => (fetchCampsites()),
     resetFeedbackForm: () => (actions.reset('feedbackForm')),
     fetchComments: () => (fetchComments()),
     fetchPromotions: () => (fetchPromotions())
 };
 
-const mapDispatchToProps = {
-    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
-    fetchCampsites: () => (fetchCampsites()),
-    resetFeedbackForm: () => (actions.reset('feedbackForm'))
-};
 
 class Main extends Component {
     
@@ -49,13 +44,7 @@ class Main extends Component {
 
         const HomePage = () => {
             return (
-                // <Home
-                //     campsite={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
-                //     campsitesLoading={this.props.campsites.isLoading}
-                //     campsitesErrMess={this.props.campsites.errMess}
-                //     promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
-                //     partner={this.props.partners.filter(partner => partner.featured)[0]}
-                // />
+                
                 <Home
                     campsite={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
                     campsitesLoading={this.props.campsites.isLoading}
@@ -70,13 +59,6 @@ class Main extends Component {
 
         const CampsiteWithId = ({match}) => {
             return (
-                // <CampsiteInfo 
-                //     campsite={this.props.campsites.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-                //     isLoading={this.props.campsites.isLoading}
-                //     errMess={this.props.campsites.errMess}
-                //     comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
-                //     addComment={this.props.addComment}
-                // />
 
                 <CampsiteInfo
                     campsite={this.props.campsites.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
@@ -84,7 +66,7 @@ class Main extends Component {
                     errMess={this.props.campsites.errMess}
                     comments={this.props.comments.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
                     commentsErrMess={this.props.comments.errMess}
-                    addComment={this.props.addComment}
+                    postComment={this.props.postComment}
                 />         
             );
         };
